2909f957f591cf3c9a359a3a11da03f65bdb1dcb
abxsky <auahabix@gmail.com>
2024-05-21 04:16:26 +0300

    setting db

--- backend/memorise_server/memorise_server/settings.py
+++ backend/memorise_server/memorise_server/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'memorise_server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST':'localhost',
+        'NAME': 'memorise_server',
+        'USER': 'root',
+        'PASSWORD': 'lij467'
     }
 }
 
